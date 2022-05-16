cf8607567079cff0132eb961d37d2fcd3ad660e0
Ruthvik-Kumar <ruthvik.phani@gmail.com>
2022-05-16 16:25:11 +0530

    next commit added logging

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 #from google.cloud import dialogflow
 from google.cloud import dialogflow_v2beta1 as dialogflow
+import logging
 
 project_id = 'newagent-hvgx'
 session_id = '123456789'  # generate random
 language_code = 'en-US'
+
+logging.basicConfig(filename= 'info.log', level= logging.DEBUG)
 
 app = Flask(__name__)
 CORS(app)
@@ -16,12 +19,12 @@ CORS(app)
 # processing the request from dialogflow
 def processRequest():
 
-    event_data = request.get_json(force=True,silent=True)
+    event_data = request.get_json(force=True, silent=True)
 
     if event_data['type'] == 'CARD_CLICKED':
         action_name = event_data['action']['actionMethodName']
         resp, button = detect_intent_texts(text=action_name)
-
+        logging.debug(resp)
     # if (req.body.type == "CARD_CLICKED"):
     #   action_name = req.body.action.actionMethodName
 
@@ -47,15 +50,12 @@ def processRequest():
                                     button
                                       }
                             }
+                        ]
                     }
                 ]
                 }
             ]
-        }
-    ]
-    }
-
-
+ }
 
 
 def detect_intent_texts(text, project_id='newagent-hvgx', session_id='123456789', language_code='en-US'):
@@ -69,20 +69,24 @@ def detect_intent_texts(text, project_id='newagent-hvgx', session_id='123456789'
     print("Session path: {}\n".format(session))
 
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
+    logging.debug(text_input)
     query_input = dialogflow.QueryInput(text=text_input)
-
+    logging.debug(query_input)
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
 
     # output_text = response.query_result.fulfillmentText
     # payload = response.query_result.fulfillmentMessages.payload
-    output = response.query_result.fulfillmentMessages
-    output_text, payload = output[0], output[2]["payload"]
+    output = response.query_result.fulfillmentText
+    output_text = output[0]
+    #payload = output[2]["payload"]['hangouts']['sections']['widgets']['buttons']
+    logging.debug(output_text)
 
-    return output_text, payload
+    return output_text
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
